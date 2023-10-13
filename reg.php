dc2f786bf8f89e17d2bb56dd3616fa3fa1718d24
Zahran4 <mahmoudhousam584@gmail.com>
2023-10-13 16:47:30 +0200

    validation

--- reg.php
+++ reg.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<style>
+.error {color:#FF0000;}
+</style>
 <html lang="en">
   <head>
     <title>Registration</title>
@@ -9,34 +12,93 @@
   </head>
   
   <body>
+    <?php
+    $nameErr =$passwordErr=$confirmErr=$birthErr= $emailErr="";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+      } else {
+        $name = test_input($_POST["name"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+          $nameErr = "Only letters and white space allowed";
+        }
+      }
+      if (empty($_POST["email"])) {
+      $emailErr = "Email is required";
+    } else {
+      $email = test_input($_POST["email"]);
+      // check if e-mail address is well-formed
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+      }
+    }
+    //check password and confirm not empty and the passwords are equal
+    if (empty($_POST["password"])) {
+      $passwordErr = "Password is required";
+    }
+
+    if (empty($_POST["confirm"])) {
+      $confirmErr = "Confirm is required";
+    } else {
+      $confirm = test_input($_POST["confirm"]);
+      // check if e-mail address is well-formed
+      if ($_POST["password"] === $_POST["confirm"]){
+      $confirmErr="";
+    }
+    else{
+      $confirmErr="passwords doesnt match";
+    }
+    }  
+ 
+if(empty($_POST["birth"])){
+  $birthErr="Enter your birth";
+}
+//Function to check if the date is valid (searching for it)
+
+    
+
+    }
+      function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    ?>
     
     <section class="container">
     <a href="index.php"><img src="imgs/Sweet Dreams logo-01.png" alt="logo" ></a>
-      <form action="#" class="form">
+      <form method="post" class="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         <div class="input-box">
           <label>Full Name</label>
-          <input type="text" placeholder="Enter full name" required />
+          <input type="text" name="name" placeholder="Enter your name" required>
+          <span class="error"> <?php echo $nameErr;?></span>
         </div>
 
         <div class="input-box">
           <label>Email Address</label>
-          <input type="text" placeholder="Enter email address" required />
+          <input type="text" name="email" placeholder="Enter email address" required />
+          <span class="error"><?php echo $emailErr;?></span>
         </div>
 
         <div class="input-box">
             <label>Password</label>
-            <input type="password" placeholder="Enter your password" required />
+            <input type="password" name="password" placeholder="Enter your password" required />
+            <span class="error"><?php echo $passwordErr;?></span>
           </div>
 
         <div class="input-box">
             <label>Confirm password</label>
-            <input type="password" placeholder="Confirm your password" required />
+            <input type="password" name="confirm" placeholder="Confirm your password" required />
+            <span class="error"><?php echo $confirmErr;?></span>
         </div>
 
         <div class="column">
           <div class="input-box">
             <label>Birth Date</label>
-            <input type="date" placeholder="Enter birth date" required />
+            <input type="date" name="birth" placeholder="Enter birth date" required />
+            <span class="error"><?php echo $birthErr;?></span>
           </div>
         </div>
         <div class="gender-box">
@@ -54,7 +116,7 @@
           </div>
         </div>
        
-        <button>Submit</button>
+      <input type="submit" name="submit" value="Submit">
       </form>
     </section>
   </body>
