4b26ef0ee310c8a5c96f3dbbc582820eb7782e8d
aboelala16 <alamokhtar42@gmail.com>
2023-10-16 23:11:07 +0300

    validation checkout

--- checkout.php
+++ checkout.php
@@ -11,6 +11,105 @@
     <link rel="icon" href="imgs/sweet dreams logo-01.png" type="image/icon type" />
     <link rel="stylesheet" href="checkout.css" />
 </head>
+<style>
+.error {color:#FF0000;}
+</style>
+
+<?php
+    
+    $fnameErr = $lnameErr = $cardNumErr = $dateErr = $emailErr = $phoneErr = $addressErr = $cvcErr = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["fname"])) {
+            $fnameErr = "First name is required";
+        } else {
+            $fname = test_input($_POST["fname"]);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z-' ]*$/", $fname)) {
+                $fnameErr = "Only letters and white space allowed";
+            }
+        }
+    
+        if (empty($_POST["lname"])) {
+            $lnameErr = "Last name is required";
+        } else {
+            $lname = test_input($_POST["lname"]);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z-' ]*$/", $lname)) {
+                $lnameErr = "Only letters and white space allowed";
+            }
+        }
+    
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is required";
+        } else {
+            $email = test_input($_POST["email"]);
+            // check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email format";
+            }
+        }
+    
+        if (empty($_POST['phone'])) {
+            $phoneErr = "Phone number is required";
+        } else {
+            $phone = test_input($_POST['phone']);
+            // Remove any non-digit characters from the phone number
+            $phone = preg_replace('/\D/', '', $phone);
+    
+            // Check if the phone number is 11 digits long
+            if (strlen($phone) === 11) {
+                echo "Phone number is valid";
+            } else {
+                $phoneErr = "Phone number is invalid";
+            }
+        }
+
+       
+
+    if (empty($_POST["address"])) {
+        $addressErr = "Address is required";
+    } else {
+        $address = test_input($_POST["address"]);
+        // Additional validation checks can be added if necessary
+    }
+
+    
+        if (empty($_POST["exp_date"])) {
+            $dateErr = "Enter your expiration date";
+        }
+
+        if (empty($_POST["cardNum"])) {
+            $cardNumErr = "Card number is required";
+        } else {
+            $cardNum = test_input($_POST["cardNum"]);
+            // Check if the card number is valid
+            if (!preg_match("/^\d{16}$/", $cardNum)) {
+                $cardNumErr = "Invalid card number";
+            }
+        }
+    
+        if (empty($_POST["CVC"])) {
+            $cvcErr = "CVC is required";
+        } else {
+            $cvc = test_input($_POST["CVC"]);
+            // Check if the CVC is a three-digit number
+            if (!preg_match("/^\d{3}$/", $cvc)) {
+                $cvcErr = "Invalid CVC";
+            }
+        }
+    
+    
+        // Function to check if the date is valid (searching for it)
+    }
+    
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
 
 <body>
     <nav>
@@ -61,42 +160,48 @@
         <div class="input-box">
                 <label>First name</label>
                 <input type="text" name="fname"  required />
+                <span class="error"><?php echo $fnameErr; ?></span>
             </div>
             <div class="input-box">
                 <label>Last name</label>
                 <input type="text" name="lname"  required />
+                <span class="error"><?php echo $lnameErr; ?></span>
             </div>
             <div class="input-box">
                 <label>Email</label>
                 <input type="text" name="email"  required />
+                <span class="error"><?php echo $emailErr; ?></span>
+
             </div>
             <div class="input-box">
                 <label>Phone number</label>
                 <input type="text" name="phone"  required />
-                
+                <span class="error"><?php echo $phoneErr; ?></span>
+
             </div>
             <div class="input-box">
                 <label>Address</label>
                 <input type="text" name="address"  required />  
+                <span class="error"><?php echo $addressErr; ?></span>
 
             </div>
             <div class="input-box">
                 <label>Card holder</label>
                 <input type="text" name="cardHolder"  required />
-                
+                <span class="error"><?php echo $cardNum; ?></span>
 
             </div>
-            <div class="input-box">
-                <label>Card number</label>
-                <input type="text" name="cardNum"  required />
-            </div>
+            
             <div class="input-box">
                 <label>Expiring date</label>
                 <input type="date" name="exp_date"  required />
+                <span class="error"><?php echo $dateErr; ?></span>
             </div>
+
             <div class="input-box">
                 <label>CVC</label>
                 <input type="text" name="CVC"  required />
+                <span class="error"><?php echo $cvcErr; ?></span>
             </div>
 
             </div>
@@ -160,8 +265,7 @@
     <p>© 2023, Sweet dreams - E-Commerce</p>
   </div>
 
-
-
+  
 </body>
 
 </html>
