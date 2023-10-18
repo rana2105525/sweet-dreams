4ff876e1223e5701afdac33ccb13966760d71179
rana2105525 <125667268+rana2105525@users.noreply.github.com>
2023-10-18 17:32:28 +0300

    profile nav bar

--- index.php
+++ index.php
@@ -17,18 +17,12 @@
 <?php
 session_start();
 
-// Check if user is logged in
-if (!isset($_SESSION['email'])) {
-    header("Location: login.php");
-    exit();
-}
-
 // Access session variables
-$email = $_SESSION['email'];
-$fullname = $_SESSION['fullname'];
+// $email = $_SESSION['email'];
+// $fullname = $_SESSION['fullname'];
 
 // Display user information
-echo "Welcome, $fullname! Your email is $email.";
+//echo "Welcome, $fullname! Your email is $email.";
 
 // Other code and HTML content for the index page
   ?>
@@ -36,16 +30,36 @@ echo "Welcome, $fullname! Your email is $email.";
     <div class="wrapper1">
       <div class="logo"><a href="index.php"><img src="imgs/sweet dreams logo-01.png" alt="logo"></a></div>
       <ul class="nav-links">
+        
+        <?php 
+         
+         if (isset($_SESSION['fullname'])) {
+             $fullname = $_SESSION['fullname'];
+             $profile = "profile.php";
+             $home="signout.php";
+             $signout="Logout";
+             echo "<li><a href=$profile;
+             >$fullname</a></li>";
+             echo "<li><a href=$home';
+             >$signout</a></li>";
+
+         } else {
+          $login="login.php";
+          $log="Login";
+          echo "<li><a href=$login;
+          >$log</a></li>";
+         }
+         
+        ?>
         <li><a href="index.php">Home</a></li>
-        <li><a href="login.php">Login</a></li>
         <li><a href="#"><i class="fa fa-shopping-bag"></i></a></li>
         <li><a href="#"><i class="fa fa-heart"></i></a></li>
 
 
       </ul>
     </div>
-    <div class="wrap">
 
+    <div class="wrap">
       <div class="search">
         <input type="text" class="searchTerm" placeholder="What are you looking for?">
         <button type="submit" class="searchButton">
