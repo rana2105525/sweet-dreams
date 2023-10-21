f211e2fca1daa2c314efaec28d74f402f2cf5843
Zahran4 <mahmoudhousam584@gmail.com>
2023-10-21 12:54:04 +0200

    terms&conditions

---
+++ t&cond.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Terms and conditions | Sweet Dreams</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+    <link rel="icon" href="imgs/sweet dreams logo-01.png" type="image/icon type" />
+    <link rel="stylesheet" href="t&cond.css" />
+</head>
+    <body>
+    <nav>
+  <?php include 'partials/nav.php'; ?>
+
+      </ul>
+    
+    </div>
+  
+    <?php include 'partials/side.php'; ?>
+</nav>
+        <section id="Terms_cond">
+            <div class="term">
+                <h1>Sweet dreams Terms and Conditions</h1>
+                <ol>
+                    <li><strong>Delivery Conditions for Sweet Dreams by HK</strong></li>
+                </ol>
+                <p>At Sweet Dreams by HK, we are committed to ensuring a seamless and delightful shopping experience for our customers. Please take a moment to review our delivery conditions to ensure that you are well informed about the order and delivery process.</p>
+                
+                <ol start="2">
+                    <li><strong>Order Confirmation Email:</strong></li>
+                </ol>  
+                   <p>You will receive an email confirmation shortly after placing your order. If you do not receive this confirmation within 5 minutes, please contact us immediately at sweetdreamsbyhk@gmail.com to ensure that your order is properly processed.</p>
+                <ol start="3">
+                    <li><strong>Delivery Timeframe:</strong></li>
+                </ol>  
+                <p>Your order will be delivered within 3-5 working days from the date of order confirmation. Please note that delivery times may vary based on your location and the availability of the product.</p>
+                <ol start="4">
+                    <li><strong>Order Cancellation:</strong></li>
+                </ol>
+                <p>You have a 2-day window to cancel your order without incurring any charges. If you decide to cancel your order, please contact us at sweetdreamsbyhk@gmail.com within this timeframe, and we will guide you through the cancellation process. Any cancellation requests made after this period may be subject to our standard cancellation policy.</p>
+                <ol start="5">
+                    <li><strong>Delayed Deliveries:</strong></li>
+                </ol>
+                <p>In the rare event that your order is not delivered within a maximum of 7 working days from the date of order confirmation, please do not hesitate to contact us at sweetdreamsbyhk@gmail.com. We will promptly investigate the delay and work to resolve the issue to your satisfaction.
+At Sweet Dreams by HK, customer satisfaction is of utmost importance to us. We strive to provide high-quality products and reliable delivery service. If you have any further questions or concerns regarding our delivery conditions or any other aspect of your shopping experience, please feel free to reach out to us at sweetdreamsbyhk@gmail.com, and our dedicated customer support team will be happy to assist you.</p>
+
+                    <strong>Thank you for choosing Sweet Dreams, where your comfort and satisfaction are our top priorities.</strong>
+               
+                    <li><strong>OWNERSHIP OF INTELLECTUAL PROPERTY</strong></li>
+                 </ol>
+                 <p>All Trademark rights in such names are hereby reserved. The use or misuse of these trademarks or any materials, except as permitted herein, is expressly prohibited, and nothing stated or implied on the sites confers on you any license or right under any patent or trademark owned or controlled by Company or any third party.</p>
+                 <p>Copyright: All information, text, images, photographs, graphics, videos, music, user interface and other content and materials contained on the sites are the copyrighted property of Company or its third-party licensors to the full extent provided under the United States Copyright Act and all international copyright laws. Under applicable copyright laws, you are prohibited from copying, reproducing, modifying, distributing, displaying, performing, or transmitting any of the contents of the sites for any purposes. Nothing stated or implied on the sites confers on you any license or right under any copyright of Company or any third party. The sites and the information contained in reference herein are for your personal, non-commercial use only. Except as otherwise specifically permitted herein, you shall not broadcast, duplicate, copy, reproduce, edit, manipulate, modify, publish, rent, sell, publicly display, perform, distribute, transmit, or circulate to anyone the contents of the sites, or use the contents of the sites in litigation, or for any commercial or promotional purposes, without the express written consent of Company or its lawful successors and assigns. </p>
+            </div>
+        </section>
+  <?php include"partials/footer.php"?>
+    </body>
+</html>
+        
