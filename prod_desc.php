baa155d7a4a2198ad3bf157b8e38adb854b4420c
Zahran4 <mahmoudhousam584@gmail.com>
2023-10-23 00:13:45 +0200

    fixed error

--- prod_desc.php
+++ prod_desc.php
@@ -27,82 +27,34 @@
     <?php include 'partials/side.php'; ?>
     </nav>
     <?php
-  if (!isset($_SESSION['description'])) {
-    $_SESSION['description'] = array();
-  }
-  if (isset($_POST['add_to_description'])) {
-    $product_id = $_POST['product_id'];
-    include_once "includes/dbh.inc.php";
-    // Retrieve the product attributes from the database based on the product ID
-    $sql = "SELECT * FROM products WHERE id = $product_id";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-
-    // Check if the wishlist array exists in the session
-  
-
-    // Add the product ID and attributes to the wishlist array
-    $_SESSION['description'][] = array(
-        'id' => $product_id,
-        'title' => $row['title'],
-        'description' => $row['description'],
-        'price' => $row['price'],
-        'prod_image' => $row['prod_image'],
-        'category' => $row['category']
-    );
-  
-}
-  ?>
-  
-
-<?php
-
-
-if (isset($_POST['submit'])) {
-
-  $fullname = $_POST['fullname'];
-  $review = $_POST['review'];
-  include_once "includes/dbh.inc.php";
-
-  $sql = "INSERT INTO reviews (fullname, review) VALUES ('$fullname', '$review');";
-
-  $result = mysqli_query($conn, $sql);
-
-  if (!$result) {
-    die(mysqli_error($conn));
-  }
-}
-
-?>
-
-    <body>
-    <?php
-    if (empty($_SESSION['description'])) {
-      echo '<div class="empty-wishlist-message">.</div>';
-    } else {
-        foreach ($_SESSION['description'] as $key => $item) {
+    if (isset($_POST['add_to_description'])) {
+        $product_id = $_POST['product_id'];
+        include_once "includes/dbh.inc.php";
+        // Retrieve the product attributes from the database based on the product ID
+        $sql = "SELECT * FROM products WHERE id = $product_id";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
     ?>
-  <div class="cont">
-    <div class="product-img">
-    <img src="<?php echo $item['prod_image']; ?>" height="420" width="327">    </div>
-    <div class="product-info">
-      <div class="product-text">
-      <h1><?php echo $item['title']; ?></h1>
-      <p><?php echo $item['description']; ?></p>
-      </div>
-      <div class="product-price-btn">
-        <p><?php echo $item['price']; ?></p>
-        <button type="button">buy now</button>
-      </div>
-    </div>
-  </div>
 
-  <?php
+        <div class="cont">
+            <div class="product-img">
+                <img src="<?php echo $row['prod_image']; ?>" height="420" width="327">
+            </div>
+            <div class="product-info">
+                <div class="product-text">
+                    <h1><?php echo $row['title']; ?></h1>
+                    <p><?php echo $row['description']; ?></p>
+                </div>
+                <div class="product-price-btn">
+                    <p><?php echo $row['price']; ?></p>
+                    <button type="button">buy now</button>
+                </div>
+            </div>
+        </div>
 
-}
+    <?php
     }
-
-?>
+    ?>
 
   <h2>Customer Reviews</h2>
   <form action="" class="form" method="post" >
