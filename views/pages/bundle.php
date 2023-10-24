004af6964eb8b8fddf3747823d93d716f3af6576
aboelala16 <alamokhtar42@gmail.com>
2023-10-25 01:24:14 +0300

    page empty

--- views/pages/bundle.php
+++ views/pages/bundle.php
@@ -13,63 +13,60 @@
 </head>
 
 <body>
-
     <nav>
-    <?php include '../partials/nav.php'; ?>
-    <?php include '../partials/side.php'; ?>
+        <?php include '../partials/nav.php'; ?>
+        <?php include '../partials/side.php'; ?>
     </nav>
     <h2>Bundle and save collection</h2>
 
-<?php
-$sql = "SELECT * FROM products WHERE category='Bundle'";
-include_once "../../config.php";
-$result = mysqli_query($conn, $sql);
-?>
-    <div class="our_Products">
-        <table>
-        <?php
-        // Loop through the products and display them dynamically
-        while ($row = mysqli_fetch_assoc($result)) {
-            $id = $row["id"];
-            $title = $row["title"];
-            $price = $row["price"];
-            $description = $row["description"];
-            $prod_image = $row["prod_image"];
-            $category = $row["category"];
-            print_r($prod_image);
-            ?>
-            <div class="products">
-            <div class="prod">
-                                <img src="<?php echo $prod_image; ?>">
+    <?php
+    $sql = "SELECT * FROM products WHERE category='Bundle'";
+    include_once "../../config.php";
+    $result = mysqli_query($conn, $sql);
+    ?>
 
-                                <div class="design">
-                                    <h5><?php echo $title; ?></h5>
-                                    <h6><?php echo $description; ?></h6>
-                                    <h6><?php echo $price; ?></h6>
-                                
-                                    <form method="post" action="wishlist.php">
-        <input type="hidden" name="product_id" value="<?php echo $row['id']; ?>">
-        <button type="submit" class="btn" name="add_to_wishlist"><i class="fa fa-heart"></i></button>
-    </form>
-    <form method="post" action="cart.php">
-        <input type="hidden" name="product_id" value="<?php echo $row['id']; ?>">
-        <button type="submit" class="btn" name="add_to_cart"><i class="fa fa-shopping-bag"></i></button>
-    
-    </form>
-                                </div>
-                            </div>
-                <?php
+    <div class="our_Products">
+        <?php
+        // Check if there are products to display
+        if (mysqli_num_rows($result) > 0) {
+            // Loop through the products and display them dynamically
+            while ($row = mysqli_fetch_assoc($result)) {
+                $id = $row["id"];
+                $title = $row["title"];
+                $price = $row["price"];
+                $description = $row["description"];
+                $prod_image = $row["prod_image"];
+        ?>
+                <div class="products">
+                    <div class="prod">
+                        <img src="<?php echo $prod_image; ?>">
+
+                        <div class="design">
+                            <h5><?php echo $title; ?></h5>
+                            <h6><?php echo $description; ?></h6>
+                            <h6><?php echo $price; ?></h6>
+
+                            <form method="post" action="wishlist.php">
+                                <input type="hidden" name="product_id" value="<?php echo $row['id']; ?>">
+                                <button type="submit" class="btn" name="add_to_wishlist"><i class="fa fa-heart"></i></button>
+                            </form>
+                            <form method="post" action="cart.php">
+                                <input type="hidden" name="product_id" value="<?php echo $row['id']; ?>">
+                                <button type="submit" class="btn" name="add_to_cart"><i class="fa fa-shopping-bag"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+        <?php
+            }
+        } else {
+            // If there are no products, display a message
+            echo "<p>No products found in the Bundle and Save collection.</p>";
         }
-        ?>      
-                  
-            
-        </table>
+        ?>
     </div>
 
     <?php include '../partials/footer.php'; ?>
-
-
-
 </body>
 
 </html>
--- views/pages/winter.php
+++ views/pages/winter.php
@@ -13,63 +13,59 @@
 </head>
 
 <body>
-
     <nav>
-    <?php include '../partials/nav.php'; ?>
-    <?php include '../partials/side.php'; ?>
+        <?php include '../partials/nav.php'; ?>
+        <?php include '../partials/side.php'; ?>
     </nav>
     <h2>Winter collection</h2>
 
-<?php
+    <?php
     $sql = "SELECT * FROM products WHERE category='Winter_Collection'";
     include_once "../.../config.php";
     $result = mysqli_query($conn, $sql);
     ?>
 
     <div class="our_Products">
-        <table>
         <?php
+        // Check if there are products to display
+        if (mysqli_num_rows($result) > 0) {
             // Loop through the products and display them dynamically
             while ($row = mysqli_fetch_assoc($result)) {
-              $id = $row["id"];
-              $title = $row["title"];
-              $price = $row["price"];
-              $description = $row["description"];
-              $prod_image = $row["prod_image"];
-              $category = $row["category"];
-            ?>
+                $id = $row["id"];
+                $title = $row["title"];
+                $price = $row["price"];
+                $description = $row["description"];
+                $prod_image = $row["prod_image"];
+                $category = $row["category"];
+        ?>
                 <div class="products">
-
-
-                <div class="prod">
-                            <img src="../../public/<?php echo $prod_image; ?>">
-
-                            <div class="design">
-                                <h5><?php echo $title; ?></h5>
-                                <h6><?php echo $description; ?></h6>
-                                <h6><?php echo $price; ?></h6>
-                                
-                                <form method="post" action="wishlist.php">
-    <input type="hidden" name="product_id" value="<?php echo $row['id']; ?>">
-    <button type="submit" class="btn" name="add_to_wishlist"><i class="fa fa-heart"></i></button>
-            </form>
-
-<form method="post" action="cart.php">
-    <input type="hidden" name="product_id" value="<?php echo $row['id']; ?>">
-    <button type="submit" class="btn" name="add_to_cart"><i class="fa fa-shopping-bag"></i></button>
-    
-</form>
-                            </div>
+                    <div class="prod">
+                        <img src="../../public/<?php echo $prod_image; ?>">
+                        <div class="design">
+                            <h5><?php echo $title; ?></h5>
+                            <h6><?php echo $description; ?></h6>
+                            <h6><?php echo $price; ?></h6>
+                            <form method="post" action="wishlist.php">
+                                <input type="hidden" name="product_id" value="<?php echo $row['id']; ?>">
+                                <button type="submit" class="btn" name="add_to_wishlist"><i class="fa fa-heart"></i></button>
+                            </form>
+                            <form method="post" action="cart.php">
+                                <input type="hidden" name="product_id" value="<?php echo $row['id']; ?>">
+                                <button type="submit" class="btn" name="add_to_cart"><i class="fa fa-shopping-bag"></i></button>
+                            </form>
                         </div>
-                    <?php
+                    </div>
+                </div>
+        <?php
             }
-            ?>
-        </table>
+        } else {
+            // If there are no products, display a message
+            echo "<p>No products found in the Winter Collection.</p>";
+        }
+        ?>
     </div>
 
     <?php include '../partials/footer.php'; ?>
-
-
 </body>
 
 </html>
